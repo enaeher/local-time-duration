94f70a813bef7934bd3f866e1b84b8dd44fa6f21
Marco Baringer <marco@onshored.com>
2014-01-28 19:17:10 +0100

    Move all the format code, and utils, into format.lisp (new file)

--- iso8601.lisp
+++ iso8601.lisp
@@ -155,35 +155,6 @@ fractional values are allowed anywhere and not only in the smallest
 value."
   (esrap:parse 'iso8601-duration string))
 
-(defun pretty-seconds (secs nsecs)
-  (if (plusp nsecs)
-      (format nil "~F" (float (+ secs (/ nsecs +nsecs-per-second+))))
-      (format nil "~D" secs)))
-
-(defun call-with-designated-stream (stream thunk)
-  (cond
-    ((null stream)
-     (with-output-to-string (string)
-       (funcall thunk string)))
-    ((eql t stream)
-     (funcall thunk *standard-output*)
-     *standard-output*)
-    ((streamp stream)
-     (funcall thunk stream)
-     stream)
-    (t
-     (error "Unable to determine the stream designated by ~S." stream))))
-
-(defmacro with-designated-stream ((var stream) &body body)
-  "Binds VAR to the stream designated (as per cl:format's stream
-argument) by the value STREAM and executes BODY.
-
-If STREAM is nil VAR will be bound to a string-output-stream and the
-resulting string will be returned, otherwise the actual stream object
-used will be returned. In either case the return value of BODY is
-ignored."
-  `(call-with-designated-stream ,stream (lambda (,var) ,@body)))
-
 (defun format-iso8601-duration (destination duration)
   (with-designated-stream (stream destination)
     (multiple-value-bind (nsecs secs minutes hours days weeks months years)
