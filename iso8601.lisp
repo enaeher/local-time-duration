0d19d7e27548c9e9914602c4e98b32617f020760
Marco Baringer <marco@onshored.com>
2014-01-28 19:02:38 +0100

    Added format-iso8601-duration; use it to implement (cl-postgres:to-sql-string duration)

--- iso8601.lisp
+++ iso8601.lisp
@@ -154,3 +154,40 @@ The only, known, divergence from the syntax specified is that
 fractional values are allowed anywhere and not only in the smallest
 value."
   (esrap:parse 'iso8601-duration string))
+
+(defun pretty-seconds (secs nsecs)
+  (if (plusp nsecs)
+      (format nil "~F" (float (+ secs (/ nsecs +nsecs-per-second+))))
+      (format nil "~D" secs)))
+
+(defun call-with-designated-stream (stream thunk)
+  (cond
+    ((null stream)
+     (with-output-to-string (string)
+       (funcall thunk string)))
+    ((eql t stream)
+     (funcall thunk *standard-output*)
+     *standard-output*)
+    ((streamp stream)
+     (funcall thunk stream)
+     stream)
+    (t
+     (error "Unable to determine the stream designated by ~S." stream))))
+
+(defmacro with-designated-stream ((var stream) &body body)
+  "Binds VAR to the stream designated (as per cl:format's stream
+argument) by the value STREAM and executes BODY.
+
+If STREAM is nil VAR will be bound to a string-output-stream and the
+resulting string will be returned, otherwise the actual stream object
+used will be returned. In either case the return value of BODY is
+ignored."
+  `(call-with-designated-stream ,stream (lambda (,var) ,@body)))
+
+(defun format-iso8601-duration (destination duration)
+  (with-designated-stream (stream destination)
+    (multiple-value-bind (nsecs secs minutes hours days weeks months years)
+        (decode-duration duration :weeks nil)
+      (declare (ignore weeks months years))
+      (write-string "P0Y0M" stream)
+      (format stream "~DDT~DH~DM~AS" days hours minutes (pretty-seconds secs nsecs)))))
