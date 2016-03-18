d0c45ebd86ee1039475399c767899044ea39edf4
Lee T Ayres <layres@gmail.com>
2016-03-18 10:32:20 -0500

    Remove some unused code

--- timestamp.lisp
+++ timestamp.lisp
@@ -1,11 +1,8 @@
 (in-package :ltd)
 
-(defun timestamp-difference (time-a time-b &key (timezone lt::*default-timezone*))
+(defun timestamp-difference (time-a time-b)
   "Returns a duration representing the time elapsed between the timestamps `TIME-A` and `TIME-B`. This duration may be negative if `TIME-B` is later than `TIME-A`."
   (let ((seconds (- (lt:timestamp-to-universal time-a) (lt:timestamp-to-universal time-b))))
-    #+nil(when lt:*use-political-time*
-      (incf seconds (- (lt::timestamp-subtimezone time-a timezone)
-		   (lt::timestamp-subtimezone time-b timezone))))    
     (duration :sec seconds)))
 
 (defun timestamp-duration+ (timestamp duration)
